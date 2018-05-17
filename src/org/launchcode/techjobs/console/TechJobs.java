bee239a5579ef36ea8a3aeda059e7925dd739e45
Christian Brinkley <christian.a.brinkley@gmail.com>
2018-05-17 18:36:36 -0500

    Finished Tech Jobs

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -61,7 +61,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -111,6 +111,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        //System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> row : someJobs) {
+            System.out.println("*****");
+            for (HashMap.Entry<String, String> entry : row.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                System.out.println(key+": "+value);
+            }
+            System.out.println("*****\n");
+        }
+        if (someJobs.size()==0)
+            System.out.println("No Results");
     }
 }
